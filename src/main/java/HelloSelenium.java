9416026e4e743941556170b0cf4f5c05daa659f0
Nina Odelius <91470981+ninaodelius@users.noreply.github.com>
2022-03-31 11:28:55 +0200

    selenium fix

--- src/main/java/HelloSelenium.java
+++ src/main/java/HelloSelenium.java
@@ -3,21 +3,16 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-
 import java.time.Duration;
-
-
 
     public class HelloSelenium {
         public static void main(String[] args) throws InterruptedException{
             WebDriverManager.chromedriver().setup();
             WebDriverManager.edgedriver().setup();
 
-
             WebDriver driver = new ChromeDriver();
 
             driver.get("https://google.com");
-
 
             System.out.println(driver.getCurrentUrl());
             System.out.println(driver.getTitle());
@@ -25,35 +20,26 @@ import java.time.Duration;
             driver.manage().timeouts().implicitlyWait(Duration.ofMillis(5000));
             Thread.sleep(5 * 1000);
 
-
-            //selenium.focus("name=Valuation"); //name of button
-            //selenium.click("Valuation");
+           // selenium.focus("name=Jag godkänner"); //name of button
+           // selenium.click("Jag godkänner");
 
             WebElement searchBox = driver.findElement(By.name("q"));
             WebElement searchButton = driver.findElement(By.name("btnK"));
-
 
             // Skriv något i textrutan
             searchBox.sendKeys("kanelbulle");
             searchButton.click();
 
             driver.getTitle();
-
             String urlValue = driver.getCurrentUrl();
 
-
-
             System.out.println(searchBox.getAttribute("value"));
-
 
             searchButton.getText();
             searchBox = driver.findElement(By.name("q"));
             searchBox.getAttribute("value"); // => "Selenium"
 
-
             driver.quit();
-
-
 
         }
     }
